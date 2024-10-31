127de44d6acae6a8a7b1be1fdaf46185b160c41e
loosimin16 <sl913@duke.edu>
2024-10-30 22:43:46 -0400

    2

--- test_main.py
+++ test_main.py
@@ -8,16 +8,16 @@ natgas = yf.Ticker("NG=F")
 df = natgas.history(interval="1d", start="2024-01-02")
 
 
-def test_load(df):
-    df = load(df)
-    assert df is not None
+def test_load():
+    df1 = load(df)
+    assert df1 is not None
 
 
-def test_summary(df):
+def test_summary():
     summary_test = summary(df)
     assert summary_test.shape == (8, 7)
 
 
 if __name__ == "__main__":
-    test_load(df)
-    test_summary(df)
+    test_load()
+    test_summary()
